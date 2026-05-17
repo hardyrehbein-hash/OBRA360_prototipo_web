dd505a3aac66172b7e7ae97a8b5ee69f501704a8
hardyrehbein-hash <hardyrehbein@gmail.com>
2026-05-17 04:35:15 -0300

    Sistema permisos por roles funcionando

--- app.js
+++ app.js
@@ -16,13 +16,40 @@ const app = document.querySelector(".app");
 const loginBox = document.getElementById("loginBox");
 const loginTitle = document.getElementById("loginTitle");
 let currentRole = "Demo";
-function abrirDashboard() {
-  loginScreen.style.display = "none";
-  loginBox.classList.remove("active");
-  app.style.display = "flex";
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
 
+  function abrirDashboard() {
+   
+    loginScreen.style.display = "none";
+    loginBox.classList.remove("active");
+    app.style.display = "flex";
+
+    document.getElementById("roleBadge").innerText =
+        currentRole === "Administrador"
+        ? "Panel Administrador"
+        : currentRole === "Cliente"
+        ? "Portal Cliente"
+        : "Dashboard Demo";
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    aplicarPermisos();
+}
+function aplicarPermisos() {
+
+    const menuPagos = document.getElementById("menuPagos");
+    const menuDocs = document.getElementById("menuDocs");
+    const menuTickets = document.getElementById("menuTickets");
+
+    if (currentRole === "Cliente") {
+
+        menuTickets.style.display = "none";
+
+    } else {
+
+        menuTickets.style.display = "block";
+
+    }
+}
 document.getElementById("enterClient").addEventListener("click", () => {
   loginBox.classList.add("active");
   loginTitle.innerText = "Acceso Cliente";
